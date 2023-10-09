82f2a97d8b458e11ac09757e7563ee688483b080
Ivo <agustinferrari15@gmail.com>
2023-10-09 15:51:41 +0200

    much cooler way to reverse string

--- csharp/reverse-string/ReverseString.cs
+++ csharp/reverse-string/ReverseString.cs
@@ -5,12 +5,13 @@ public static class ReverseString
 {
     public static string Reverse(string input)
     {
-        string reverseWord = "";
+        /*string reverseWord = "";
         for (int i = input.Length - 1; i >= 0; i--)
         {
             reverseWord += input[i];
         }
-        return reverseWord;
+        return reverseWord;*/
+        return new String(input.Reverse().ToArray());
         //throw new NotImplementedException("You need to implement this function.");
     }
 }
